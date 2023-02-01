0f90fc6a02389af0ed90f908f0d94f90f2e2b203
8401483508 <prashantbhai22000@student.hamk.fi>
2023-02-01 10:53:48 +0200

    exercise 1 submitt

--- layout/index.php
+++ layout/index.php
@@ -3,7 +3,7 @@
 <h1> some content </h1>
 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam repudiandae aperiam maiores neque non tempora adipisci libero, nesciunt ipsa sed explicabo sapiente nihil, praesentium architecto quidem est iusto perferendis quisquam.</p>
 <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis rem perspiciatis minima corrupti omnis nostrum odit ipsa iusto nemo suscipit, aliquam ullam repellendus, consequatur reprehenderit inventore beatae, mollitia quis explicabo?</p>
-
+<p> Lorem </p>
 
 
 
