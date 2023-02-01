3613693a651543728b8ab0a600b4a90deef7052a
8401483508 <prashantbhai22000@student.hamk.fi>
2023-02-01 11:28:44 +0200

    some changes

--- layout/ex1.php
+++ layout/ex1.php
@@ -1,4 +1,4 @@
-    <?php include "header.php"?>
+<?php include "header.php"?>
     <h1 style="font-size:18px ;" >3.1 Write a simple PHP script to print your information (Name and your groupid).</h1>
     <?php
     echo "My name is Prashant.";
@@ -12,4 +12,39 @@
     <?php
     echo date("d.m.y") ?>
 
+    <h3> 3.4 variable in php </h3>
+    <?php
+    $title = "PHP is interesting";
+    echo "<h1> " . $title1 . "</h1>";
+    ?>
+
+    <h3> 3.5 Table & Variable </h3>
+    <?php
+    $g1 = 5;
+    $g2 = 4;
+    $g3 = 5;
+    echo "
+    <table>
+    <tr>
+    <th> S.N </th><th> Name <th><th> Grade </th>
+    </tr>
+    <tr>
+    <td> 1 </td><td> pekka </td><td. $g1 </td>
+    </tr>
+    <tr>
+    <td> 2 </td><td> johanna </td><td. $g2 </td>
+    </tr>
+    <tr>
+    <td> 3 </td><td> john </td><td. $g3 </td>
+    </tr>
+    </table>
+    ";
+    ?>
+    <h3> 4 screenshot of the development environment </h3>
+    <img src="2.png" alt="">
+
+
+
     <?php include "footer.php"?>
+
+    
